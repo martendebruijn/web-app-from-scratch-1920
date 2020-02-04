bf3c99b74e1402d204d4913553bec439b2aeb9e5
Marten de Bruijn <martenbruijn@gmail.com>
2020-02-04 11:49:19 +0100

    added color

--- docs/js/app.js
+++ docs/js/app.js
@@ -3,43 +3,46 @@ let argument = 'involvedMaker=Rembrandt+van+Rijn';
 let objects = [];
 let i = 0;
 
-fetch(`https://www.rijksmuseum.nl/api/nl/collection?key=${key}&involvedMaker=Vincent+van+Gogh`)
-  .then((response) => {
+fetch(
+  `https://www.rijksmuseum.nl/api/nl/collection?key=${key}&involvedMaker=Vincent+van+Gogh`
+)
+  .then(response => {
     return response.json();
   })
-  .then((myJson) => {
+  .then(myJson => {
     myJson.artObjects.forEach(element => objects.push(element.objectNumber));
-    objects.forEach(element => 
-        fetch(`https://www.rijksmuseum.nl/api/nl/collection/${element}?key=${key}`)
-        .then((response2) => {
-        return response2.json();
+    objects.forEach(element =>
+      fetch(
+        `https://www.rijksmuseum.nl/api/nl/collection/${element}?key=${key}`
+      )
+        .then(response2 => {
+          return response2.json();
         })
-        .then((myJson2) => {
-            if (myJson2.artObject.colors.length > 0) {
+        .then(myJson2 => {
+          if (myJson2.artObject.colors.length > 0) {
             let result = document.createElement('div');
+            let colorWrapper = document.createElement('div');
             result.id = `result${i}`;
+            colorWrapper.id = `wrapper${i}`;
+            colorWrapper.classList.add('colorWrapper');
             document.body.appendChild(result);
             let title = document.createElement('h2');
-            let bar = document.createElement('div');
-            bar.classList.add('bar');
             title.innerHTML = myJson2.artObject.title;
-            myJson2.artObject.colors.forEach(element => { 
-
-                // COLOR DIV WERKT NOG NIET SOEPEL
-                let colorDiv = document.createElement('div');
-                colorDiv.style.backgroundColor = element.hex;
-                colorDiv.style.width = '20px';
-                colorDiv.style.height = '20px';
-                let res = document.querySelector(`#result${i}`)
-                res.querySelectorAll(".bar").appendChild(colorDiv)
-                })
             document.querySelector(`#result${i}`).appendChild(title);
-            document.querySelector(`#result${i}`).appendChild(bar);
+            document.querySelector(`#result${i}`).appendChild(colorWrapper);
+            for (
+              let z = 0, length = myJson2.artObject.colors.length;
+              z < length;
+              z++
+            ) {
+              let colorBlock = document.createElement('div');
+              colorBlock.classList.add('colorBlock');
+              colorBlock.style.backgroundColor =
+                myJson2.artObject.colors[z].hex;
+              document.querySelector(`#wrapper${i}`).appendChild(colorBlock);
+            }
             i++;
-        }
-        }))
+          }
+        })
+    );
   });
-
-  
-    
-

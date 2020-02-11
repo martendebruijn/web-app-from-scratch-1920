774a935855a0fbb24571ce62788b6fcd178efbda
Marten de Bruijn <martenbruijn@gmail.com>
2020-02-11 14:13:26 +0100

    fixed hex value

--- docs/js/app.js
+++ docs/js/app.js
@@ -6,14 +6,6 @@ const rgbToHex = function(rgb) {
   return hex;
 };
 
-// const fullColorHex = function(r, g, b) {
-//   const red = rgbToHex(r);
-//   const green = rgbToHex(g);
-//   const blue = rgbToHex(b);
-//   // return red + green + blue;
-//   console.log(red + green + blue);
-// };
-
 const changeFullColor = function() {
   const redSlider = document.querySelector('#red');
   const redOutput = document.querySelector('#redOutput');
@@ -22,6 +14,7 @@ const changeFullColor = function() {
   const blueSlider = document.querySelector('#blue');
   const blueOutput = document.querySelector('#blueOutput');
   let resultHex = '';
+
   let colorObject = {
     red: redSlider.value,
     green: greenSlider.value,
@@ -33,7 +26,13 @@ const changeFullColor = function() {
       outputEl.innerText = this.value;
       colorObject[key] = rgbToHex(this.value);
       resultHex = colorObject.red + colorObject.green + colorObject.blue;
-      console.log(resultHex);
+      Object.keys(colorObject).forEach(key => {
+        if (colorObject[key] == 0) {
+          colorObject[key] = '00';
+        }
+      });
+      console.log(colorObject);
+      console.log('#' + resultHex);
     };
   }
   changeValue(redSlider, redOutput);

19528091648eb9d6f5c9d6db56e0c6a63c67606f
Marten de Bruijn <martenbruijn@gmail.com>
2020-02-11 15:22:25 +0100

    progress app.js

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,3 +1,8 @@
+/* TO DO:
+[] REWRITE THIS CODE BELOW
+[] PASS THE RESULT HEX ON TO THE API FETCH
+
+*/
 const rgbToHex = function(rgb) {
   let hex = Number(rgb).toString(16);
   if (hex.length < 2) {
@@ -6,7 +11,7 @@ const rgbToHex = function(rgb) {
   return hex;
 };
 
-const changeFullColor = function() {
+const changeFullColor = (function() {
   const redSlider = document.querySelector('#red');
   const redOutput = document.querySelector('#redOutput');
   const greenSlider = document.querySelector('#green');
@@ -27,17 +32,13 @@ const changeFullColor = function() {
       colorObject[key] = rgbToHex(this.value);
       resultHex = colorObject.red + colorObject.green + colorObject.blue;
       Object.keys(colorObject).forEach(key => {
-        if (colorObject[key] == 0) {
-          colorObject[key] = '00';
-        }
+        colorObject[key] = ('0' + colorObject[key]).slice(-2);
       });
-      console.log(colorObject);
-      console.log('#' + resultHex);
+      const resultEl = document.querySelector('.result-color');
+      resultEl.style.backgroundColor = '#' + resultHex;
     };
   }
   changeValue(redSlider, redOutput);
   changeValue(greenSlider, greenOutput);
   changeValue(blueSlider, blueOutput);
-};
-
-changeFullColor();
+})();

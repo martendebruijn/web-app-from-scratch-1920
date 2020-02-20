fcd1a375e2e5045580432a9b40e91ae7aa68bd37
Marten de Bruijn <martenbruijn@gmail.com>
2020-02-20 21:04:10 +0100

    tried routie

--- docs/js/modules/appModule.js
+++ docs/js/modules/appModule.js
@@ -1,10 +1,15 @@
 import { color } from './color.js';
 import { api } from './api.js';
+// import { router } from './routieModule.js';
 
 export const appModule = {
   init: function() {
     color.fullValue();
     const searchBtn = document.querySelector('#searchBtn');
     searchBtn.addEventListener('click', api.requestPaintings);
+
+    // routie('nijlpaard', function() {
+    //   document.querySelector('#nijlpaard').classList.toggle('d-none');
+    // });
   },
 };
--- docs/js/modules/routieModule.js
+++ docs/js/modules/routieModule.js
@@ -2,3 +2,15 @@
 [] learn Routie
 [] add Routie
 */
+
+export const router = {
+  nijl: function() {
+    if (window.location.hash == 'nijlpaard') {
+      console.log('hoi dit werkt');
+    }
+  },
+};
+// routie('nijlpaard', function() {
+//   //this gets called when hash == #users
+//   console.log('routie -> users');
+// });
